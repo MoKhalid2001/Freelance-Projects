8410ab6e53299890f375f54dffae6a4ad8eaebcc
MoKhalid2001 <mokhalid2252001@gmail.com>
2024-07-14 18:48:24 +0300

    initialtelegrambot

---
+++ Telegram Bot/BotBackend/src/main/java/com/mycompany/botbackend/BotBackend.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.botbackend;
+
+/**
+ *
+ * @author mk225
+ */
+public class BotBackend {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
